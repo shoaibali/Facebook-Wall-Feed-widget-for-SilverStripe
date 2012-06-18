e887b65a7c4baf6dae3346bc34a378c9097fc6c0
Shoaib Ali <hacker.nz@gmail.com>
2012-06-18 23:40:44 +1200

    added better CSS and language file nl_NL

---
+++ lang/nl_NL.php
@@ -0,0 +1,22 @@
+<?php
+
+global $lang;
+
+$lang['nl_NL']['FacebookFeedWidget']['DESCRIPTION'] = 'Een lijst van de meest recente facebook berichten, video, links, foto etc';
+$lang['nl_NL']['FacebookFeedWidget']['Heading'] = 'Omschrijving titel';
+$lang['nl_NL']['FacebookFeedWidget']['LIMIT'] = 'Maximum aantal berichten om te laten zien';
+$lang['nl_NL']['FacebookFeedWidget']['PLURALNAME'] = array(
+	'Facebook Wall Stream Widgets',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['nl_NL']['FacebookFeedWidget']['SINGULARNAME'] = array(
+	'Facebook Wall Stream Widget',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['nl_NL']['FacebookFeedWidget']['TITLE'] = 'Facebook Wall Stream';
+$lang['nl_NL']['FacebookFeedWidget.ss']['UPDATE'] = 'Facebook Update';
+$lang['nl_NL']['FacebookFeedWidget.ss']['UPDATES'] = 'Facebook Updates';
+
+?>
